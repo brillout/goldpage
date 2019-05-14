5955f078384b4a8e4fe03c3aeba99b3197361389
Romuald Brillout <github.com@brillout.com>
2019-05-14 15:24:16 +0200

    wip

--- plugins/webpack/index.js
+++ plugins/webpack/index.js
@@ -36,18 +36,12 @@ module.exports = {
         transparentGetter('transpileServerCode'),
         transparentGetter('log'),
         {
-            prop: 'webpackBrowserConfigModifier',
-            getter: configParts => assemble_modifiers('webpackBrowserConfig', configParts),
-        },
-        {
-            prop: 'webpackNodejsConfigModifier',
-            getter: configParts => assemble_modifiers('webpackNodejsConfig', configParts),
-        },
-        {
             prop: 'browserConfigs',
             getter: getBrowserConfigs,
         },
         arrayGetter('browserInitFunctions'),
+        arrayGetter('webpackNodejsConfig'),
+        arrayGetter('webpackBrowserConfig'),
     ],
     runBuildFile,
     getBuildInfoFile,
@@ -55,47 +49,6 @@ module.exports = {
     getPageBrowserEntriesFile,
     ejectables: getEjectables(),
 };
-
-// We assemble several webpack config modifiers into one supra modifier
-function assemble_modifiers(modifier_name, configParts) {
-    const assert_usage = require('reassert/usage');
-
-    // `configParts` holds all globalConfig parts
-    // `config` holds a webpack config
-    let supra_modifier = ({config}) => config;
-
-    // We assemble all `configParts`'s config modifiers into one `supra_modifier`
-    configParts
-    .forEach(configPart => {
-        const modifier = configPart[modifier_name];
-        if( ! modifier ) {
-            return;
-        }
-        assert_usage(configPart[modifier_name] instanceof Function);
-        const previous_modifier = supra_modifier;
-        supra_modifier = (
-            args => {
-                const config = previous_modifier(args);
-                const config__new = modifier({...args, config});
-                assert_usage(
-                    config__new,
-                    (
-                        configPart.$name ? (
-                            "The `"+modifier_name+"` of `"+configPart.$name+"`"
-                        ) : (
-                            "A `"+modifier_name+"`"
-                        )
-                    ) + (
-                        " is returning `"+config__new+"` but it should be returning a webpack config instead."
-                    )
-                );
-                return config__new;
-            }
-        );
-    });
-
-    return supra_modifier;
-}
 
 function getBrowserConfigs(configParts) {
     const assert_plugin = require('reassert/usage');
--- plugins/webpack/runBuild.js
+++ plugins/webpack/runBuild.js
@@ -1,12 +1,19 @@
 const Build = require('webpack-ssr/Build');
 const watchDir = require('webpack-ssr/watchDir');
+const assert = require('reassert');
 
 const projectConfig = require('@brillout/reconfig').getConfig({configFileName: 'reframe.config.js'});
 
 const outputDir = projectConfig.projectFiles.buildOutputDir;
 const getPageFiles = () => projectConfig.getPageConfigFiles();
-const getWebpackBrowserConfig = ({config, ...utils}) => projectConfig.webpackBrowserConfigModifier({config, ...utils});
-const getWebpackNodejsConfig = ({config, ...utils}) => projectConfig.webpackNodejsConfigModifier({config, ...utils});
+const getWebpackBrowserConfig = ({config, ...utils}) => {
+  const webpackBrowserConfigModifier = assemble_modifiers('webpackBrowserConfig'),
+  webpackBrowserConfigModifier({config, ...utils});
+};
+const getWebpackNodejsConfig = ({config, ...utils}) => {
+  const webpackNodejsConfigModifier = assemble_modifiers('webpackNodejsConfig'),
+  webpackNodejsConfigModifier({config, ...utils});
+};
 const {log, doNotWatchBuildFiles} = projectConfig;
 const {pagesDir} = projectConfig.projectFiles;
 const {getPageHtmls, getPageBrowserEntries} = projectConfig;
@@ -27,3 +34,37 @@ const build = new Build({
 watchDir(pagesDir, () => {build()});
 
 module.exports = build;
+
+
+
+// We assemble several webpack config modifiers into one supra modifier
+function assemble_modifiers(modifier_name) {
+    const assert_usage = require('reassert/usage');
+
+    // `config` holds a webpack config
+    let supra_modifier = ({config}) => config;
+
+    const modifiers = projectConfig[modifier_name];
+
+    // We assemble all `configParts`'s config modifiers into one `supra_modifier`
+    modifiers
+    .forEach(modifier => {
+        assert_usage(modifier instanceof Function, modifier);
+        const previous_modifier = supra_modifier;
+        supra_modifier = (
+            args => {
+                const config = previous_modifier(args);
+                const config__new = modifier({...args, config});
+                assert_usage(
+                    config__new,
+                    "A `"+modifier_name+"`" +
+                    " is returning `"+config__new+"` but it should be returning a webpack config instead."
+                );
+                return config__new;
+            }
+        );
+    });
+
+    return supra_modifier;
+}
+
