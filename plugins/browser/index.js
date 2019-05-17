b713aac41c757e2fb20018adc82eede06d8320ad
Romuald Brillout <github.com@brillout.com>
2019-05-17 12:40:15 +0200

    wip

--- plugins/browser/index.js
+++ plugins/browser/index.js
@@ -9,54 +9,19 @@ Object.assign(
     browserInitFile,
     browserInitFunctions: AppendArray([
       {
-          name: 'hydratePage',
-          initFunctionFile: hydratePageFile,
-          doNotInclude: ({pageConfig}) => !!pageConfig.doNotRenderInBrowser,
-          // -50 is fairly aggressive to ensure that hydration is
-          // one of the first thing that happens in the browser
-          executionOrder: -50,
-          browserConfigsNeeded: [
-              'renderPageToDom',
-              'renderToDom',
-              'pageConfig',
-              'router',
-          ],
+        name: 'hydratePage',
+        initFunctionFile: hydratePageFile,
+        doNotInclude: ({pageConfig}) => !!pageConfig.doNotRenderInBrowser,
+        // -50 is fairly aggressive to ensure that hydration is
+        // one of the first thing that happens in the browser
+        executionOrder: -50,
+        browserConfigsNeeded: [
+          'renderPageToDom',
+          'renderToDom',
+          'pageConfig',
+          'router',
+        ],
       },
     ]),
   },
 );
-
-/*
-module.exports = {
-    ejectables: [
-        {
-            name: 'browser',
-            description: 'Eject the browser initialization code.',
-            actions: [
-                {
-                    targetDir: 'browser/',
-                    configIsFilePath: true,
-                    configPath: 'browserInitFile',
-                },
-            ],
-        },
-        {
-            name: 'browser-hydrate',
-            description: 'Eject the code that orchestrates the hydration of the page.',
-            actions: [
-                {
-                    targetDir: 'browser/',
-                    configPath: 'browserInitFunctions',
-                    configIsList: true,
-                    listElementKeyProp: 'name',
-                    listElementKey: 'hydratePage',
-                    newConfigValue: ({copyCode, oldConfigValue}) => ({
-                        ...oldConfigValue,
-                        initFunctionFile: copyCode(oldConfigValue.initFunctionFile),
-                    }),
-                }
-            ],
-        },
-    ],
-};
-*/
--- plugins/path-to-regexp/index.js
+++ plugins/path-to-regexp/index.js
@@ -8,19 +8,3 @@ Object.assign(
     browserConfigs: AppendArray(['routerFile']),
   },
 );
-
-    /*
-    ejectables: [
-        {
-            name: 'router',
-            description: 'Eject the `@reframe/path-to-regepx` router.',
-            actions: [
-                {
-                    targetDir: 'router/',
-                    configIsFilePath: true,
-                    configPath: 'routerFile',
-                },
-            ],
-        },
-    ],
-    */
