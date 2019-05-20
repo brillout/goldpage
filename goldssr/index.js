71462c01427651fee0ffae3bc14072f78c216d31
Romuald Brillout <github.com@brillout.com>
2019-05-20 16:39:08 +0200

    improve interface

--- goldssr/index.js
+++ goldssr/index.js
@@ -28,90 +28,85 @@ function create_ssr() {
   //*/
 
   return new GoldSSR();
-}
 
-function GoldSSR() {
-  const {projectDir, findProjectFiles} = new ProjectFiles();
+  function GoldSSR() {
+    const {projectDir, findProjectFiles} = new ProjectFiles();
 
-  config.GoldSSR.getPageConfigFiles = () => {
-    assert.usage(
-      ssr.pagesDir,
-      "You need to set `pagesDir`",
+    config.GoldSSR.getPageConfigFiles = () => {
+      assert.usage(
+        ssr.pagesDir,
+        "You need to set `pagesDir`",
+      );
+      const pageConfigs = findProjectFiles('*\.page-config\.*', {within: ssr.pagesDir+'/ewqeui'});
+      assert.usage(
+        pageConfigs.length>=1,
+        "No files with the `.page-config.` suffix found in `"+ssr.pagesDir+"`.",
+        "You need to define at least one page config file.",
+      );
+      return pageConfigs;
+    };
+
+    this.build = build;
+    Object.assign(
+      this,
+      config.GoldSSR.serverAdapters,
+      {build},
     );
-    const pageConfigs = findProjectFiles('*\.page-config\.*', {within: ssr.pagesDir+'/ewqeui'});
-    assert.usage(
-      pageConfigs.length>=1,
-      "No files with the `.page-config.` suffix found in `"+ssr.pagesDir+"`.",
-      "You need to define at least one page config file.",
-    );
-    return pageConfigs;
-  };
 
-  this.build = build;
-  Object.assign(
-    this,
-    config.GoldSSR.serverAdapters,
-    {build},
-  );
+    return new Proxy(this, {set, get});
 
-  return new Proxy(this, {set, get});
-
-  function set(_, prop, value) {
-    assert.internal(_===ssr);
-
-    if( ['pagesDir', 'buildDir'].includes(prop) ){
-      value = path.resolve(projectDir, value);
-    }
-
-    ssr[prop] = value;
-
-    if( prop==='log' ){
-      prop = 'logOptions';
-    }
-
-    config.GoldSSR[prop] = value;
-
-    return true;
-  }
-
-  function get(_, prop) {
-    assert.internal(_===ssr);
-
-    if( !prop in ssr ) {
-      const server_adapter_getter = config.GoldSSR.serverAdapters['get_'+prop];
-      if( server_adapter_getter ) {
-        return server_adapter_getter;
+    function set(ssr_obj, prop, value) {
+      if( ['pagesDir', 'buildDir'].includes(prop) ){
+        value = path.resolve(projectDir, value);
       }
+
+      ssr_obj[prop] = value;
+
+      if( prop==='log' ){
+        prop = 'logOptions';
+      }
+
+      config.GoldSSR[prop] = value;
+
+      return true;
     }
 
-    return ssr[prop];
+    function get(ssr_obj, prop) {
+      if( !(prop in ssr_obj) ) {
+        const server_adapter_getter = config.GoldSSR.serverAdapters['get_'+prop];
+        if( server_adapter_getter ) {
+          return server_adapter_getter();
+        }
+      }
+
+      return ssr_obj[prop];
+    }
   }
-}
 
-async function build() {
+  async function build() {
+    /*
+    assert.usage(
+      loadedPlugins.length>0,
+      "No GoldSSR Plugins loaded. Add some to "+packageJsonFile,
+    );
+    */
+    const buildConfigMissing = !config.GoldSSR.runBuild;
+    assert.usage(
+      buildConfigMissing,
+      {loadedPlugins},
+      "A builder plugin is missing. Add one, such as `@goldssr/webpack`, to "+packageJsonFile,
+    );
+    const runBuild = require(config.GoldSSR.runBuildFile);
+    await runBuild();
+  }
+
   /*
-  assert.usage(
-    loadedPlugins.length>0,
-    "No GoldSSR Plugins loaded. Add some to "+packageJsonFile,
-  );
+  function assert_reconfig() {
+    assert_usage(
+      config.GoldSSR.rend,
+      {loadedPlugins},
+      "A builder plugin is missing. Add one, such as `@goldssr/webpack`, to "+packageJsonFile,
+    );
+  }
   */
-  const buildConfigMissing = !config.GoldSSR.runBuild;
-  assert.usage(
-    buildConfigMissing,
-    {loadedPlugins},
-    "A builder plugin is missing. Add one, such as `@goldssr/webpack`, to "+packageJsonFile,
-  );
-  const runBuild = require(config.GoldSSR.runBuildFile);
-  await runBuild();
-}
-
-/*
-function assert_reconfig() {
-  assert_usage(
-    config.GoldSSR.rend,
-    {loadedPlugins},
-    "A builder plugin is missing. Add one, such as `@goldssr/webpack`, to "+packageJsonFile,
-  );
-}
-*/
 }
