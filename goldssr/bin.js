a3b3f40d2445f69524d6579924b673d25fae9af3
Romuald Brillout <github.com@brillout.com>
2019-05-24 19:11:15 +0200

    wip

--- goldssr/bin.js
+++ goldssr/bin.js
@@ -1,9 +1,23 @@
 #!/usr/bin/env node
 
-const {argv} = process;
-console.log(argv);
-if( true ) {
-  require('./dev');
-} else {
-  require('./build');
-}
+const assert = require('@brillout/reassert');
+const arg = process.argv.slice(2)[0];
+
+(() => {
+  if( arg==='dev' ){
+    assert.usage(
+      process.env.NODE_ENV!=='production',
+    );
+    require('./dev');
+    return;
+  }
+  if( arg==='build' ){
+    process.env.NODE_ENV='production';
+    require('./build');
+    return;
+  }
+  assert.usage(
+    false,
+    "Unknown argument `"+arg+"`."
+  );
+})();
