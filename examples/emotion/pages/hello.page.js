b901ad0f503c8d0fbaffaf3bf988b9e4950de0d9
Romuald Brillout <github.com@brillout.com>
2019-06-24 20:49:10 +0200

    add emotion example

---
+++ examples/emotion/pages/hello.page.js
@@ -0,0 +1,29 @@
+import React from 'react';
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core'
+
+export default {
+  view: Hello,
+  route: '/',
+};
+
+function Hello() {
+  const color = 'white'
+
+  return (
+    <div
+      css={css`
+        padding: 32px;
+        background-color: hotpink;
+        font-size: 24px;
+        border-radius: 4px;
+        width: 250px;
+        &:hover {
+          color: ${color};
+        }
+      `}
+    >
+      Hover to change color.
+    </div>
+  );
+}
