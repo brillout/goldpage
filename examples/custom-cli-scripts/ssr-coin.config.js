aa4dd728f5586fc5a32f54a17177330f0ecb1fd7
Romuald Brillout <github.com@brillout.com>
2019-07-01 17:53:13 +0200

    improve examples

---
+++ examples/custom-cli-scripts/ssr-coin.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  silent: true,
+  /*
+  pagesDir: __dirname+'/pages',
+  log: {
+    buildingText: 'Building...',
+    builtText: 'All built',
+    showLoadingSpinner: true,
+    verbose: false,
+  },
+  */
+  serverEntryFile: __dirname+'/server',
+};
