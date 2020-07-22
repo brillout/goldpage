06e7d692d593fcd3a7404f6b1891184b000b501a
Romuald Brillout <github.com@brillout.com>
2020-07-22 19:42:07 +0200

    show donation-reminder

--- plugins/html/renderPageToDom.js
+++ plugins/html/renderPageToDom.js
@@ -1,8 +1,17 @@
+const { printDonationReminder } = require("@lsos/donation-reminder");
 const browserConfig = require("@brillout/browser-config");
 const CONTAINER_ID = require("./CONTAINER_ID");
 const assert = require("@brillout/reassert");
 
 module.exports = renderPageToDom;
+
+printDonationReminder({
+  npmName: "@goldpage",
+  projectName: "Goldpage",
+  donationText:
+    "Bonjour :slight_smile:, I am Romuald, any help is motivating me a lot :heart:",
+  minNumberOfAuthors: 0,
+});
 
 async function renderPageToDom({ initialProps }) {
   assert.internal(initialProps);
