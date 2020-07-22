c2892fb51c6d5ba1e2e033db39309fa246e6f447
Romuald Brillout <github.com@brillout.com>
2020-07-22 19:20:43 +0200

    apply prettier

--- plugins/html/renderPageToDom.js
+++ plugins/html/renderPageToDom.js
@@ -1,10 +1,10 @@
-const browserConfig = require('@brillout/browser-config');
-const CONTAINER_ID = require('./CONTAINER_ID');
-const assert = require('@brillout/reassert');
+const browserConfig = require("@brillout/browser-config");
+const CONTAINER_ID = require("./CONTAINER_ID");
+const assert = require("@brillout/reassert");
 
 module.exports = renderPageToDom;
 
-async function renderPageToDom({initialProps}) {
+async function renderPageToDom({ initialProps }) {
   assert.internal(initialProps);
 
   await browserConfig.domRender({
